72b4ea1b472a8b75ffa1314a943c5d108be1a25e
Shirodevolop <emilioelva11@gmail.com>
2022-05-20 09:33:19 +0700

    Anko’s doAsync

--- app/src/main/java/com/android/pertemuan_4_bagian_12_threaddanrunnable/MainActivity.kt
+++ app/src/main/java/com/android/pertemuan_4_bagian_12_threaddanrunnable/MainActivity.kt
@@ -3,24 +3,21 @@ package com.android.pertemuan_4_bagian_12_threaddanrunnable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
-
+import org.jetbrains.anko.activityUiThread
+import org.jetbrains.anko.doAsync
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         button.setOnClickListener {
-            Thread(Runnable {
-                killSomeTime()
-            }).start()
-        }
-    }
-    private fun killSomeTime() {
-        for (i in 1..20) {
-            runOnUiThread(Runnable{
-                textView.text = i.toString()
-            })
-            println("i:$i")
-            Thread.sleep(2000)
+            doAsync {
+                for(i in 1..15) {
+                    Thread.sleep(2000)
+                    activityUiThread {
+                        textView.text = i.toString()
+                    }
+                }
+            }
         }
     }
 }
